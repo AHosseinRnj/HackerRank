efbe01e7f234a766cba6706a0c184ac7f906471a
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-09-16 16:35:58 +0330

    Add Time Conversion Problem

---
+++ Week 1/3. Time Conversion/TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace TimeConversion
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'timeConversion' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string timeConversion(string s)
+        {
+            return String.Empty;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.timeConversion(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
