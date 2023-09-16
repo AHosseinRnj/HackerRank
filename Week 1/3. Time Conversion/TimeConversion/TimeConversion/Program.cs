4abc813d50fc8bae5ab210050f51dd13902a7fe3
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-09-16 17:25:13 +0330

    Add Time Conversion Solution

--- Week 1/3. Time Conversion/TimeConversion/TimeConversion/Program.cs
+++ Week 1/3. Time Conversion/TimeConversion/TimeConversion/Program.cs
@@ -26,7 +26,32 @@ namespace TimeConversion
 
         public static string timeConversion(string s)
         {
-            return String.Empty;
+            /// Using DateTime :
+
+            /*
+            DateTime dateTime;
+            DateTime.TryParse(s, out dateTime);
+
+            return dateTime.TimeOfDay.ToString();
+            */
+
+            /// Without using DateTime :
+
+            var input = s.Split(':');
+
+            var hour = Convert.ToInt32(input[0]);
+            var minute = Convert.ToInt32(input[1]);
+            var second = Convert.ToInt32(input[2].Substring(0, 2));
+            var meridiem = input[2].Substring(2, 2);
+
+            if (meridiem == "PM" && hour != 12)
+                hour += 12;
+            else if (meridiem == "AM" && hour == 12)
+                hour = 0;
+
+            var result = string.Format("{0:D2}:{1:D2}:{2:D2}", hour, minute, second);
+
+            return result;
         }
     }
 
@@ -34,7 +59,7 @@ namespace TimeConversion
     {
         static void Main(string[] args)
         {
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            TextWriter textWriter = new StreamWriter("C:\\Users\\AmirHossein\\Desktop\\Test\\text.txt", true);
 
             string s = Console.ReadLine();
 
@@ -44,6 +69,8 @@ namespace TimeConversion
 
             textWriter.Flush();
             textWriter.Close();
+
+            Console.ReadLine();
         }
     }
 }
