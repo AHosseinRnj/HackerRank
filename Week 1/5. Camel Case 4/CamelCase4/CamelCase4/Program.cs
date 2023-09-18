df081e4ee4fb7dd0844cd4c8688ef74f77021e98
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-09-18 08:56:01 +0330

    Add Camel Case 4 Problem

---
+++ Week 1/5. Camel Case 4/CamelCase4/CamelCase4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CamelCase4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+        }
+    }
+}
