6183dd4b51898320ef3d2a66b25f7c035717ec49
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-09-27 09:37:22 +0330

    Add Flipping bits Problem

---
+++ Week 2/3. Flipping Bits/FlippingBits/FlippingBits/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace FlippingBits
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'flippingBits' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts LONG_INTEGER n as parameter.
+         */
+
+        public static long flippingBits(long n)
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+                long result = Result.flippingBits(n);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
