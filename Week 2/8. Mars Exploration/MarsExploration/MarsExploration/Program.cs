64c12861ae50dc75d1dfbd924122d0bd2170471a
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-07 21:33:20 +0330

    Add Mars Exploration Problem

---
+++ Week 2/8. Mars Exploration/MarsExploration/MarsExploration/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace MarsExploration
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'marsExploration' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static int marsExploration(string input)
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            int result = Result.marsExploration(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
