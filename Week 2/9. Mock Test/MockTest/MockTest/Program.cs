8a040998eebb79caeef66bc27102e38c11e127fa
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-08 08:40:17 +0330

    Add Mock Test Problem

---
+++ Week 2/9. Mock Test/MockTest/MockTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace MockTest
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'flippingMatrix' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+         */
+
+        public static int flippingMatrix(List<List<int>> matrix)
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<List<int>> matrix = new List<List<int>>();
+
+                for (int i = 0; i < 2 * n; i++)
+                {
+                    matrix.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(matrixTemp => Convert.ToInt32(matrixTemp)).ToList());
+                }
+
+                int result = Result.flippingMatrix(matrix);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
