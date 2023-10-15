158962a2d4ea77b0591b2122bb76ef93b398047b
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-15 18:25:48 +0330

    Add Maximum Perimeter Triangle Problem

---
+++ Week 3/6. Maximum Perimeter Triangle/MaximumPerimeterTriangle/MaximumPerimeterTriangle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace MaximumPerimeterTriangle
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'maximumPerimeterTriangle' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY sticks as parameter.
+         */
+
+        public static List<int> maximumPerimeterTriangle(List<int> sticks)
+        {
+            return new List<int>();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> sticks = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sticksTemp => Convert.ToInt32(sticksTemp)).ToList();
+
+            List<int> result = Result.maximumPerimeterTriangle(sticks);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
