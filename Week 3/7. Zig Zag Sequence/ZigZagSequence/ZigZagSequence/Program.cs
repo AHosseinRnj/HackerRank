49004bb6091f4d8d03c3e1f72626a5aad582c06b
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-16 09:38:51 +0330

    Add Zig Zag Sequence Problem

---
+++ Week 3/7. Zig Zag Sequence/ZigZagSequence/ZigZagSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZigZagSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+        }
+
+        public static void FindZigZagSequence(int[] a, int n)
+        {
+
+        }
+    }
+}
