9c27a9957db64259998325ddd00b3d55d78c5542
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-16 09:50:42 +0330

    Add Solution

--- Week 3/7. Zig Zag Sequence/ZigZagSequence/ZigZagSequence/Program.cs
+++ Week 3/7. Zig Zag Sequence/ZigZagSequence/ZigZagSequence/Program.cs
@@ -15,7 +15,29 @@ namespace ZigZagSequence
 
         public static void FindZigZagSequence(int[] a, int n)
         {
+            Array.Sort(a);
+            var mid = n / 2;
+            var temp = a[mid];
+            a[mid] = a[n - 1];
+            a[n - 1] = temp;
 
+            var st = mid + 1;
+            var ed = n - 2;
+            while (st <= ed)
+            {
+                temp = a[st];
+                a[st] = a[ed];
+                a[ed] = temp;
+                st++;
+                ed--;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 0)
+                    Console.Write(" ");
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine();
         }
     }
 }
