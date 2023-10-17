de31c50be02ddd5ec09e7cfaef66bd5519dcbdaa
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-17 15:44:59 +0330

    Add Drawing Book Problem

---
+++ Week 3/8. Drawing Book/DrawingBook/DrawingBook/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace DrawingBook
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'pageCount' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER p
+         */
+
+        public static int pageCount(int n, int p)
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.pageCount(n, p);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
