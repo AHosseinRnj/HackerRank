029fd52f24b6d0fdda0fca145d71f33e50640dd3
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-21 23:26:11 +0330

    Add Solution

--- Week 4/1. Picking Numbers/PickingNumbers/PickingNumbers/Program.cs
+++ Week 4/1. Picking Numbers/PickingNumbers/PickingNumbers/Program.cs
@@ -12,7 +12,55 @@
 
         public static int pickingNumbers(List<int> a)
         {
-            return 0;
+            Validate(a);
+
+            a.Sort();
+
+            var maxLenght = 0;
+            var freqMap = new Dictionary<int, int>();
+
+            foreach (var number in a)
+            {
+                if (freqMap.ContainsKey(number))
+                    freqMap[number]++;
+                else
+                    freqMap[number] = 1;
+            }
+
+            foreach (var number in freqMap.Keys)
+            {
+                var currLenght = freqMap[number];
+
+                if (freqMap.ContainsKey(number + 1))
+                    currLenght += freqMap[number + 1];
+
+                maxLenght = Math.Max(maxLenght, currLenght);
+            }
+
+            return maxLenght;
+
+
+            //var max = int.MinValue;
+            //for (int i = 0; i < a.Count; i++)
+            //{
+            //    var countOfFirstElement = a.Where(val => val == i).Count();
+            //    var countOfSecondElement = a.Where(val => val == i - 1).Count();
+            //    var total = countOfFirstElement + countOfSecondElement;
+
+            //    if(total > max)
+            //        max = total;
+            //}
+
+            //return max;
+        }
+
+        private static void Validate(List<int> a)
+        {
+            if (a.Count < 2 || a.Count > 100)
+                throw new ArgumentException("Array size should be between 2 and 100", nameof(a.Count));
+
+            if (a.Any(val => val < 0 || val > 100))
+                throw new ArgumentException("Each array elements should be between 0 and 100", nameof(a));
         }
     }
 
