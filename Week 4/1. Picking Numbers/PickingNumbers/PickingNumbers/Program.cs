1fda8c3de7e7482038cc45f1db03f948934a93d5
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-21 21:53:23 +0330

    Add Picking Numbers Problem

---
+++ Week 4/1. Picking Numbers/PickingNumbers/PickingNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PickingNumbers
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'pickingNumbers' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY a as parameter.
+         */
+
+        public static int pickingNumbers(List<int> a)
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            int result = Result.pickingNumbers(a);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
