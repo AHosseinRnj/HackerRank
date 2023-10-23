56ee51d08a2ca6209aaf6f76c95bf17673df9d5a
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-23 22:26:36 +0330

    Add Solution

--- Week 4/2. Left Rotation/LeftRotation/LeftRotation/Program.cs
+++ Week 4/2. Left Rotation/LeftRotation/LeftRotation/Program.cs
@@ -14,7 +14,48 @@
 
         public static List<int> rotateLeft(int d, List<int> arr)
         {
-            return new List<int>();
+            Validate(d, arr);
+
+            // 1st Way O(N + N):
+            /*
+            var tempList = new List<int>();
+            for (int i = d; i < arr.Count; i++)
+                tempList.Add(arr[i]);
+
+            for (int i = 0; i < d; i++)
+                tempList.Add(arr[i]);
+
+            return tempList;
+            */
+
+            // 2nd Way O(d * N) - ** Time limit **:
+            var shiftCount = 1;
+            var totalShift = d;
+
+            while (shiftCount <= totalShift)
+            {
+                var firstElement = arr[0];
+                for (var i = 0; i < arr.Count - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Count - 1] = firstElement;
+                shiftCount++;
+            }
+
+            return arr;
+        }
+
+        private static void Validate(int d, List<int> arr)
+        {
+            if (arr.Count < 1 || arr.Count > Math.Pow(10, 5))
+                throw new ArgumentException("Array length should be between 1 and 10^5", nameof(arr.Count));
+
+            if (d < 1 || d > arr.Count)
+                throw new ArgumentException("The amount to rotate, should be between 1 and n", nameof(d));
+
+            if (arr.Any(val => val < 1 || val > Math.Pow(10, 6)))
+                throw new ArgumentException("Each array elements must be between 1 and 10^6");
         }
     }
 
