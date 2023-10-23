893925d33dfbd034821ef39379fb7027de9144dd
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-23 16:44:20 +0330

    Add Left Rotation Problem

---
+++ Week 4/2. Left Rotation/LeftRotation/LeftRotation/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LeftRotation
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'rotateLeft' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER d
+         *  2. INTEGER_ARRAY arr
+         */
+
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            return new List<int>();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int d = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> result = Result.rotateLeft(d, arr);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
