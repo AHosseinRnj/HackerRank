96991e739f1290c33767b9b2f68a4ad0ceab21ff
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-26 19:01:59 +0330

    Add Separate the Numbers Problem

---
+++ Week 4/4. Separate the Numbers/SeparateTheNumbers/SeparateTheNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SeparateTheNumbers
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'separateNumbers' function below.
+         *
+         * The function accepts STRING s as parameter.
+         */
+
+        public static void separateNumbers(string s)
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                Result.separateNumbers(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
