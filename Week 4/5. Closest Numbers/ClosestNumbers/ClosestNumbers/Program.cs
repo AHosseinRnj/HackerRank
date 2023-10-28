5cd7702bd8f6e0f1db70e0b961089bc2a258b5c2
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-28 20:58:37 +0330

    Add Closet Numbers Problem

---
+++ Week 4/5. Closest Numbers/ClosestNumbers/ClosestNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ClosestNumbers
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'closestNumbers' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static List<int> closestNumbers(List<int> arr)
+        {
+            return new List<int>();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> result = Result.closestNumbers(arr);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
