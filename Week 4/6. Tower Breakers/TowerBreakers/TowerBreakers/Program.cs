797b1faf442791356f94e8238fbf5a9775de52d1
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-29 14:44:45 +0330

    Add Tower Breakers Problem

---
+++ Week 4/6. Tower Breakers/TowerBreakers/TowerBreakers/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace TowerBreakers
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'towerBreakers' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER m
+         */
+
+        public static int towerBreakers(int n, int m)
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+                int n = Convert.ToInt32(firstMultipleInput[0]);
+
+                int m = Convert.ToInt32(firstMultipleInput[1]);
+
+                int result = Result.towerBreakers(n, m);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
