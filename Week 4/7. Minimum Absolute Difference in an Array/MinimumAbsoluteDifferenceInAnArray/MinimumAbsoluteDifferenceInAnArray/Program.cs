8767f603ea271bab6b53b7ac20c96f04dca837b7
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-30 22:19:39 +0330

    Add Minimum Absolute Difference in an Array Problem

---
+++ Week 4/7. Minimum Absolute Difference in an Array/MinimumAbsoluteDifferenceInAnArray/MinimumAbsoluteDifferenceInAnArray/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MinimumAbsoluteDifferenceInAnArray
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'minimumAbsoluteDifference' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int minimumAbsoluteDifference(List<int> arr)
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = Result.minimumAbsoluteDifference(arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
