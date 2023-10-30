8424d5ab601291e81dbc2194d64eaaf8652c656e
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-30 22:31:51 +0330

    Add Solution

--- Week 4/7. Minimum Absolute Difference in an Array/MinimumAbsoluteDifferenceInAnArray/MinimumAbsoluteDifferenceInAnArray/Program.cs
+++ Week 4/7. Minimum Absolute Difference in an Array/MinimumAbsoluteDifferenceInAnArray/MinimumAbsoluteDifferenceInAnArray/Program.cs
@@ -12,7 +12,32 @@
 
         public static int minimumAbsoluteDifference(List<int> arr)
         {
-            return 0;
+            Validate(arr);
+
+            arr.Sort();
+
+            var minDifference = Int32.MaxValue;
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+                var firstNum = arr[i];
+                var secondNum = arr[i + 1];
+
+                var absoluteDifference = Math.Abs(firstNum - secondNum);
+
+                if (absoluteDifference < minDifference)
+                    minDifference = absoluteDifference;
+            }
+
+            return minDifference;
+        }
+
+        private static void Validate(List<int> arr)
+        {
+            if (arr.Count < 2 || arr.Count > Math.Pow(10, 5))
+                throw new ArgumentException("Array length should be between 2 and 10^5", nameof(arr.Count));
+
+            if (arr.Any(val => val < Math.Pow(-10, 9) || val > Math.Pow(10, 9)))
+                throw new ArgumentException("Each array elements must be between -10^9 and 10^9", nameof(arr));
         }
     }
 
