28a8552976df9ec1901a97252306b68ef5c8973d
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-31 08:30:53 +0330

    Add Caesar Cipher Problem

---
+++ Week 4/8. Caesar Cipher/CaesarCipher/CaesarCipher/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CaesarCipher
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'caesarCipher' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts following parameters:
+         *  1. STRING s
+         *  2. INTEGER k
+         */
+
+        public static string caesarCipher(string s, int k)
+        {
+            return string.Empty;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string s = Console.ReadLine();
+
+            int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string result = Result.caesarCipher(s, k);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
