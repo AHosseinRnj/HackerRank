b2a4256915ec5ec4fadd677902435444b4e2bd62
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-02 19:50:02 +0330

    Add Problem

---
+++ Week 4/9. Mock Test/MockTest/MockTest/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MockTest
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'anagram' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static int anagram(string s)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = Result.anagram(s);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
