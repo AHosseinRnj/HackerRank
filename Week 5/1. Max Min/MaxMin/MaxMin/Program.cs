9c3e5102cc1056ee56dfb7ba392144a66dd227fb
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-04 21:38:31 +0330

    Add Max Min Problem

---
+++ Week 5/1. Max Min/MaxMin/MaxMin/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MaxMin
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'maxMin' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER k
+         *  2. INTEGER_ARRAY arr
+         */
+
+        public static int maxMin(int k, List<int> arr)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int arrItem = Convert.ToInt32(Console.ReadLine().Trim());
+                arr.Add(arrItem);
+            }
+
+            int result = Result.maxMin(k, arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
