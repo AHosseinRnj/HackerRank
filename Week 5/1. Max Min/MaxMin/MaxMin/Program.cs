28c3d0df6d56561f484b2ef8459f851547d1954d
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-04 23:03:42 +0330

    Add Solution

--- Week 5/1. Max Min/MaxMin/MaxMin/Program.cs
+++ Week 5/1. Max Min/MaxMin/MaxMin/Program.cs
@@ -14,7 +14,35 @@
 
         public static int maxMin(int k, List<int> arr)
         {
-            return 0;
+            Validate(k, arr);
+
+            arr.Sort();
+            var arrayLength = arr.Count;
+            var minUnfairness = int.MaxValue;
+
+            for (int i = 0; i <= arrayLength - k; i++)
+            {
+                // Max - Min
+                var tempUnfairness = arr[i + k - 1] - arr[i];
+
+                if (tempUnfairness < minUnfairness)
+                    minUnfairness = tempUnfairness;
+            }
+
+            return minUnfairness;
+        }
+
+        private static void Validate(int k, List<int> arr)
+        {
+            var arraySize = arr.Count;
+            if (arraySize < 2 || arraySize > Math.Pow(10, 5))
+                throw new ArgumentException("Array size should be between 2 and 10^5", nameof(arraySize));
+
+            if (k < 2 || k > arraySize)
+                throw new ArgumentException("K, Should be between 2 and n", nameof(k));
+
+            if (arr.Any(val => val < 0 || val > Math.Pow(10, 9)))
+                throw new ArgumentException("Each array elements must be between 0 and 10^9", nameof(arr));
         }
     }
 
