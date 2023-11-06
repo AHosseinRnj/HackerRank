cb031bb9b924429f99bd6a31f950bf3d3d4d0d68
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-06 08:24:53 +0330

    Add Dynamic Array

---
+++ Week 5/3. Dynamic Array/DynamicArray/DynamicArray/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DynamicArray
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'dynamicArray' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. 2D_INTEGER_ARRAY queries
+         */
+
+        public static List<int> dynamicArray(int n, List<List<int>> queries)
+        {
+            return new List<int>();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int q = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<List<int>> queries = new List<List<int>>();
+
+            for (int i = 0; i < q; i++)
+            {
+                queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+            }
+
+            List<int> result = Result.dynamicArray(n, queries);
+
+            textWriter.WriteLine(String.Join("\n", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
