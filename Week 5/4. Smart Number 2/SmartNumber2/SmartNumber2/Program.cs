00bff383fce32f076a1ed78ff75e38ec79c7e271
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-07 08:55:01 +0330

    Add Smart Number Problem

---
+++ Week 5/4. Smart Number 2/SmartNumber2/SmartNumber2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartNumber2
+{
+    public class Program
+    {
+        // Debug Problem.
+    }
+
+    class Result
+    {
+        public static bool isSmartNumber(int num)
+        {
+            int val = (int)Math.Sqrt(num);
+            if (num / val == 1)
+                return true;
+            return false;
+        }
+    }
+}
