04b5496bf6306dd69fe620db0ff7b1236c50e1e7
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-08 18:09:37 +0330

    Add The Full Counting Sort Problem

---
+++ Week 5/6. The Full Counting Sort/TheFullCountingSort/TheFullCountingSort/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TheFullCountingSort
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'countSort' function below.
+         *
+         * The function accepts 2D_STRING_ARRAY arr as parameter.
+         */
+
+        public static void countSort(List<List<string>> arr)
+        {
+
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<string>> arr = new List<List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList());
+            }
+
+            Result.countSort(arr);
+        }
+    }
+}
