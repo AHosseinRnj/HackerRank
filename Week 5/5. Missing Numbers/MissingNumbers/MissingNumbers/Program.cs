5d80325b0505624064a7d78eeb94bbb171380e6b
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-08 09:28:47 +0330

    Add Missing Numbers Problem

---
+++ Week 5/5. Missing Numbers/MissingNumbers/MissingNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MissingNumbers
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'missingNumbers' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY arr
+         *  2. INTEGER_ARRAY brr
+         */
+
+        public static List<int> missingNumbers(List<int> arr, List<int> brr)
+        {
+            return new List<int>();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int m = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+            List<int> result = Result.missingNumbers(arr, brr);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
