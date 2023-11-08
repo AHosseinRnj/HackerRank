e8b9711cd7ea5a1298dcaca328e8b4a7a6822a36
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-08 19:40:42 +0330

    Add Solution

--- Week 5/6. The Full Counting Sort/TheFullCountingSort/TheFullCountingSort/Program.cs
+++ Week 5/6. The Full Counting Sort/TheFullCountingSort/TheFullCountingSort/Program.cs
@@ -11,7 +11,60 @@
 
         public static void countSort(List<List<string>> arr)
         {
+            Validate(arr);
 
+            var mid = arr.Count / 2;
+            var result = new List<List<string>>();
+            for (int i = 0; i < 100; i++)
+            {
+                result.Add(new List<string>());
+            }
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                var index = int.Parse(arr[i][0]);
+                var value = arr[i][1];
+
+                if (i < mid)
+                    result[index].Add("-");
+                else
+                    result[index].Add(value);
+            }
+
+
+            /*
+            var output = string.Join(" ", result.Select(val => string.Join(" ", val))).Trim();
+            Console.WriteLine(output);
+            */
+
+            foreach (var strings in result)
+                foreach (var str in strings)
+                    Console.Write(str + " ");
+        }
+
+        private static void Validate(List<List<string>> arr)
+        {
+            var arrCount = arr.Count;
+            if (arrCount % 2 != 0)
+                throw new ArgumentException("The number of pairs must be even", nameof(arrCount));
+
+            if (arrCount < 1 || arrCount > 1000000)
+                throw new ArgumentException("The number of pairs must be between 1 and 1000000", nameof(arrCount));
+
+            for (int i = 0; i < arrCount; i++)
+            {
+                var number = int.Parse(arr[i][0]);
+                var characters = arr[i][1];
+
+                if (number < 0 || number > 100)
+                    throw new ArgumentException("The integer value must be between 0 and 100", nameof(number));
+
+                if (characters.Length < 1 || characters.Length > 10)
+                    throw new ArgumentException("he length of each string must be between 1 and 10", nameof(characters));
+
+                if (!characters.Any(char.IsLower))
+                    throw new ArgumentException("Each string must consist of lowercase letters only", nameof(characters));
+            }
         }
     }
 
