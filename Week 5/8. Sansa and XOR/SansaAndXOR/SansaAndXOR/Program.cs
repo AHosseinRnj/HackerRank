1f9f6474ebe8012e441cf98d996531a23ec3dde2
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-13 22:41:56 +0330

    Add Sansa and XOR Problem

---
+++ Week 5/8. Sansa and XOR/SansaAndXOR/SansaAndXOR/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SansaAndXOR
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'sansaXor' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int sansaXor(List<int> arr)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+                int result = Result.sansaXor(arr);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
