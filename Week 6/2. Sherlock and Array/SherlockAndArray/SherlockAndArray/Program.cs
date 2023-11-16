85a91fdf670d060fca388b8ebde652ecce4afb3d
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-16 22:22:53 +0330

    Add Problem

---
+++ Week 6/2. Sherlock and Array/SherlockAndArray/SherlockAndArray/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SherlockAndArray
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'balancedSums' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static string balancedSums(List<int> arr)
+        {
+            return string.Empty;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int T = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int TItr = 0; TItr < T; TItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+                string result = Result.balancedSums(arr);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
