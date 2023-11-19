63b4f131462597f4ee60354dd968e0becb99f299
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-19 21:55:17 +0330

    Add Solution

---
+++ Week 6/3. Misere Nim/MisereNim/MisereNim/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MisereNim
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'misereNim' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts INTEGER_ARRAY s as parameter.
+         */
+
+        public static string misereNim(List<int> s)
+        {
+            return string.Empty;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+                string result = Result.misereNim(s);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
