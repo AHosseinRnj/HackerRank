20a4c43925f774300b7452fae3706d6e6ffe9420
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-20 16:57:36 +0330

    Add Gaming Array 1 Problem

---
+++ Week 6/4. Gaming Array 1/GamingArray1/GamingArray1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GamingArray1
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'gamingArray' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static string gamingArray(List<int> arr)
+        {
+            return string.Empty;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int g = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int gItr = 0; gItr < g; gItr++)
+            {
+                int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+                string result = Result.gamingArray(arr);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
