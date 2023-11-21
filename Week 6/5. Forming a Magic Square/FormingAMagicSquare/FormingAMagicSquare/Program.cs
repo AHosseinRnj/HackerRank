342af195146d1d08599e23931ce9387a70c1c340
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-21 10:08:14 +0330

    Add Forming a Magic Square Problem

---
+++ Week 6/5. Forming a Magic Square/FormingAMagicSquare/FormingAMagicSquare/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace FormingAMagicSquare
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'formingMagicSquare' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY s as parameter.
+         */
+
+        public static int formingMagicSquare(List<List<int>> s)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<List<int>> s = new List<List<int>>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                s.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList());
+            }
+
+            int result = Result.formingMagicSquare(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
