a685d16724dab5c94503d474d52b13aa75df32cb
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-21 09:49:44 +0330

    Add Solution

--- Week 6/4. Gaming Array 1/GamingArray1/GamingArray1/Program.cs
+++ Week 6/4. Gaming Array 1/GamingArray1/GamingArray1/Program.cs
@@ -12,7 +12,65 @@
 
         public static string gamingArray(List<int> arr)
         {
-            return string.Empty;
+            Validate(arr);
+
+            /*
+                - ANDY = 1
+                - BOB = 2
+            */
+
+            /* Time Limit :
+            var currentPlayer = 1;
+
+            while (true)
+            {
+                var arrayLength = arr.Count;
+                if (arrayLength == 0)
+                    break;
+
+                var maxElement = arr.Max();
+                var maxElementIndex = arr.IndexOf(maxElement);
+                var removeCount = arrayLength - maxElementIndex;
+
+                arr.RemoveRange(maxElementIndex, removeCount);
+
+                currentPlayer = currentPlayer == 1 ? 2 : 1;
+            }
+
+            if (currentPlayer == 1)
+                return "ANDY";
+
+            return "BOB";
+            */
+
+            var count = 1;
+            var lastMax = arr[0];
+
+            for (var i = 1; i < arr.Count; i++)
+            {
+                if (arr[i] > lastMax)
+                {
+                    lastMax = arr[i];
+                    count++;
+                }
+            }
+
+            if (count % 2 == 0)
+                return "ANDY";
+
+            return "BOB";
+        }
+
+        private static void Validate(List<int> arr)
+        {
+            if (arr.Count != arr.Distinct().Count())
+                throw new ArgumentException("Array should not contains duplicates", nameof(arr));
+
+            if (arr.Count < 1 || arr.Count > Math.Pow(10, 5))
+                throw new ArgumentException("Array length should be between 1 and 10^5", nameof(arr));
+
+            if (arr.Any(val => val < 1 || val > Math.Pow(10, 9)))
+                throw new ArgumentException("Each array elements must be between 1 and 10^9", nameof(arr));
         }
     }
 
