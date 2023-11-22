c445441f868b0ec15af2460952d2789c20002ae2
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-22 08:57:16 +0330

    Add Recursive Digit Sum Problem

---
+++ Week 6/6. Recursive Digit Sum/RecursiveDigitSum/RecursiveDigitSum/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace RecursiveDigitSum
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'superDigit' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. STRING n
+         *  2. INTEGER k
+         */
+
+        public static int superDigit(string n, int k)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            string n = firstMultipleInput[0];
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.superDigit(n, k);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
