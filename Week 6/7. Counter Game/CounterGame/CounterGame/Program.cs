26e544a0245433c26acf331bd332303c3aece367
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-22 10:30:36 +0330

    Add Counter Game Problem

---
+++ Week 6/7. Counter Game/CounterGame/CounterGame/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CounterGame
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'counterGame' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts LONG_INTEGER n as parameter.
+         */
+
+        public static string counterGame(long n)
+        {
+            return string.Empty;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+                string result = Result.counterGame(n);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
