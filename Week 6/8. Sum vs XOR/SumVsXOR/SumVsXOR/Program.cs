ad2b236786d9c76e0c8671ca6890cfb6dcc70c9d
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-26 08:36:45 +0330

    Add Sum vs XOR Problem

---
+++ Week 6/8. Sum vs XOR/SumVsXOR/SumVsXOR/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SumVsXOR
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'sumXor' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts LONG_INTEGER n as parameter.
+         */
+
+        public static long sumXor(long n)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+            long result = Result.sumXor(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
