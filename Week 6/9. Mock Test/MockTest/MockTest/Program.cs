266347d9826e5ba09bcebf19e687a97dcdbce18a
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-27 08:38:15 +0330

    Add Mock Test Problem

---
+++ Week 6/9. Mock Test/MockTest/MockTest/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MockTest
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'palindromeIndex' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static int palindromeIndex(string s)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = Result.palindromeIndex(s);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
