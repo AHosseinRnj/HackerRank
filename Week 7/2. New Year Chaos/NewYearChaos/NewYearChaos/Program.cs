248a681690b3aaf3d578682de4799d48003ca310
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-12-01 14:57:42 +0330

    Add Solution

--- Week 7/2. New Year Chaos/NewYearChaos/NewYearChaos/Program.cs
+++ Week 7/2. New Year Chaos/NewYearChaos/NewYearChaos/Program.cs
@@ -11,7 +11,46 @@
 
         public static void minimumBribes(List<int> q)
         {
+            Validate(q);
 
+            var swapCount = 0;
+            for (int i = q.Count - 1; i >= 0; i--)
+            {
+                if (q[i] != i + 1)
+                {
+                    if (q[i - 1] == i + 1)
+                    {
+                        swapCount++;
+                        Swap(q, i, i - 1);
+                    }
+                    else if (q[i - 2] == i + 1)
+                    {
+                        swapCount += 2;
+                        Swap(q, i - 2, i - 1);
+                        Swap(q, i - 1, i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too chaotic");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine(swapCount);
+        }
+
+        private static void Swap(List<int> q, int firstIndex, int secondIndex)
+        {
+            int temp = q[firstIndex];
+            q[firstIndex] = q[secondIndex];
+            q[secondIndex] = temp;
+        }
+
+        private static void Validate(List<int> q)
+        {
+            if (q.Count < 1 || q.Count > Math.Pow(10, 5))
+                throw new ArgumentException("Array length should be between 1 and 10^5", nameof(q));
         }
     }
 
