627a11c90ad7627550b3074ac6164cde7ff77859
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-12-01 13:46:17 +0330

    Add New Year Chaos Problem

---
+++ Week 7/2. New Year Chaos/NewYearChaos/NewYearChaos/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NewYearChaos
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'minimumBribes' function below.
+         *
+         * The function accepts INTEGER_ARRAY q as parameter.
+         */
+
+        public static void minimumBribes(List<int> q)
+        {
+
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<int> q = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(qTemp => Convert.ToInt32(qTemp)).ToList();
+
+                Result.minimumBribes(q);
+            }
+        }
+    }
+}
