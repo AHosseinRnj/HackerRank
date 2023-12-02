b7fe9254a9bad62e7a7343955d568687de96ca87
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-12-02 12:06:15 +0330

    Add Goodland Electricity Problem

---
+++ Week 7/3. Goodland Electricity/GoodlandElectricity/GoodlandElectricity/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace GoodlandElectricity
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'pylons' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER k
+         *  2. INTEGER_ARRAY arr
+         */
+
+        public static int pylons(int k, List<int> arr)
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = Result.pylons(k, arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
