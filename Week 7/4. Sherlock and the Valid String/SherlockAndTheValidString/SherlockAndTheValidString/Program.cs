2f9806d828cb162fe7c4473599834a8511ff6955
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-12-03 08:43:25 +0330

    Add Sherlock and the Valid String Problem

---
+++ Week 7/4. Sherlock and the Valid String/SherlockAndTheValidString/SherlockAndTheValidString/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SherlockAndTheValidString
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'isValid' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string isValid(string s)
+        {
+            return string.Empty;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.isValid(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
