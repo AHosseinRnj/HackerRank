a5cef97666fa59c1b795877f1401cad09965c315
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-12-04 09:08:59 +0330

    Add Climbing the Leaderboard Problem

---
+++ Week 7/5. Climbing the Leaderboard/ClimbingTheLeaderboard/ClimbingTheLeaderboard/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ClimbingTheLeaderboard
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'climbingLeaderboard' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY ranked
+         *  2. INTEGER_ARRAY player
+         */
+
+        public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+        {
+            return new List<int>();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+            int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+            List<int> result = Result.climbingLeaderboard(ranked, player);
+
+            textWriter.WriteLine(String.Join("\n", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
