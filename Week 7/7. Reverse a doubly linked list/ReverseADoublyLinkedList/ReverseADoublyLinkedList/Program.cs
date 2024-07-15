3e014d1794216fe111f511a487c055ff02ab290b
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2024-07-15 20:28:44 +0330

    Add reverse a doubly linked list question

---
+++ Week 7/7. Reverse a doubly linked list/ReverseADoublyLinkedList/ReverseADoublyLinkedList/Program.cs
@@ -0,0 +1,96 @@
+﻿class Solution
+{
+    class DoublyLinkedListNode
+    {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    class DoublyLinkedList
+    {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintDoublyLinkedList(DoublyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    class Result
+    {
+        public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist)
+        {
+            return new DoublyLinkedListNode(0);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            DoublyLinkedListNode llist1 = Result.reverse(llist.head);
+
+            PrintDoublyLinkedList(llist1, " ", textWriter);
+            textWriter.WriteLine();
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
